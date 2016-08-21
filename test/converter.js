d5faa84622e996a0f1b87d3af8316c4efb65a364
Warrick FitzGerald <warrick.fitzgerald@gmail.com>
2016-08-21 14:21:23 -0400

    add tests

---
+++ test/converter.js
@@ -0,0 +1,16 @@
+var expect    = require("chai").expect;
+var converter = require("../app/converter");
+
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+    	expect(1+1).to.equal(2);
+    });
+  });
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+		expect(1+1).to.equal(2);
+    });
+  });
+});
